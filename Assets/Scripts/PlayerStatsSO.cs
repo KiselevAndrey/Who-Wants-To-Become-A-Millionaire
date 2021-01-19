151f7d74a3c06158b1ea8522f295ffdf09d43bbe
KiselevAndrey <andrey-199213@mail.ru>
2021-01-19 22:58:37 +0300

    Revert "Revert "v1.0.1""
    This reverts commit 7089b7732eb742aff83e0247b78fd4d497b1f422.

--- Assets/Scripts/PlayerStatsSO.cs
+++ Assets/Scripts/PlayerStatsSO.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu(fileName = "PlayerStats", menuName = "Статы игрока")]
 public class PlayerStatsSO : ScriptableObject
